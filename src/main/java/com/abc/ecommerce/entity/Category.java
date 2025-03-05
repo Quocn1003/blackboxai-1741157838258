db5fb944bca20366663cb65d30f3502a12d2578d
Quocn1003 <93835013+Quocn1003@users.noreply.github.com>
2025-03-05 13:57:52 +0700

    Add src/main/java/com/abc/ecommerce/entity/Category.java 1741157862433

---
+++ src/main/java/com/abc/ecommerce/entity/Category.java
@@ -0,0 +1,31 @@
+package com.abc.ecommerce.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Category name is required")
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    private String image;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
+}
